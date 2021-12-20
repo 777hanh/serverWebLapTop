3ea26ca46550172c7bd1a68835096db738744d09
nongdanxomdua <thanhthientran1511@gmail.com>
2021-12-21 00:54:09 +0700

    fix some wrong

--- src/routes/authStaff.js
+++ src/routes/authStaff.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const staffController = require('../app/controllers/StaffController')
+const staffController = require('../app/controllers/staffController')
 
 router.post('/getUsers', staffController.getAll)
 router.post('/login', staffController.login)

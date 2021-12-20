7ef72188907b3cb0f2eede9c2907e3009bc08d7b
nongdanxomdua <thanhthientran1511@gmail.com>
2021-12-21 01:04:24 +0700

    fix text

--- src/routes/authStaff.js
+++ src/routes/authStaff.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const staffController = require('../app/controllers/staffController')
 
-router.post('/getUsers', staffController.getAll)
+router.post('/getusers', staffController.getAll)
 router.post('/login', staffController.login)
 router.post('/register', staffController.register)
 
--- src/routes/authUser.js
+++ src/routes/authUser.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const userController = require('../app/controllers/userController')
 
-router.post('/getUsers', userController.getAll)
+router.post('/getusers', userController.getAll)
 router.post('/login', userController.login)
 router.post('/register', userController.register)
 
